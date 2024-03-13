25434b51bb7166282a47bc3b51fc2286fcb9fac7
Hassanbhb <Hassan.benhajbouih@gmail.com>
2024-03-13 12:42:24 +0000

    added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ export default {
     "./resources/**/*.blade.php",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vermilion': {
+          '50': '#fff4ed',
+          '100': '#ffe6d4',
+          '200': '#ffc8a8',
+          '300': '#ffa270',
+          '400': '#ff6f37',
+          '500': '#ff460c',
+          '600': '#f02e06',
+          '700': '#c71e07',
+          '800': '#9e190e',
+          '900': '#7f190f',
+          '950': '#450805',
+        },          
+      },
+    },
   },
   plugins: [],
 }
