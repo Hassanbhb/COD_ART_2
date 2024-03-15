79312a5b7ade593fc8cde2bc990cb7f81fd527ad
Hassanbhb <Hassan.benhajbouih@gmail.com>
2024-03-15 13:56:20 +0000

    fix: select icon bug

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -95,15 +95,7 @@
                                                 <option>Option 3</option>
                                                 <option>Option 4</option>
                                             </select>
-                                            <div
-                                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-400">
-                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                    viewbox="0 0 20 20">
-                                                    <path
-                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z">
-                                                    </path>
-                                                </svg>
-                                            </div>
+                                            
                                         </div>
                                     </div>
                                     <div class="mb-6">
@@ -149,15 +141,6 @@
                                                 <option>$5k - $10k</option>
                                                 <option>$10k +</option>
                                             </select>
-                                            <div
-                                                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-400">
-                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                    viewbox="0 0 20 20">
-                                                    <path
-                                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z">
-                                                    </path>
-                                                </svg>
-                                            </div>
                                         </div>
                                     </div>
                                     <div class="mb-6">
