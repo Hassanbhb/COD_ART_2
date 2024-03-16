870d9b22bb9f06587435373d2338e71abfd3040b
Hassanbhb <Hassan.benhajbouih@gmail.com>
2024-03-16 11:02:42 +0000

    fix: broken link

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,18 @@
 <nav class="relative bg-white mb-12 md:mb-24">
     <div class="container mx-auto px-4">
         <div class="relative flex h-24 items-center">
-            <a class="inline-block" href="#">
-                <img class="h-10" src="{{ Vite::asset('resources/assets/logos/aurora-logo.svg') }}"
-                    alt="">
+            <a class="inline-block" href="{{ route('home') }}">
+                <img class="h-10" src="{{ Vite::asset('resources/assets/logos/aurora-logo.svg') }}" alt="">
             </a>
             <button
                 class="lg:hidden navbar-burger flex items-center justify-center h-10 w-10 ml-auto bg-gradient-to-b from-cyanGreen-800 to-cyan-800 rounded-lg">
-                <svg width="24" height="24" viewbox="0 0 24 24" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M3 5H21" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                        stroke-linejoin="round"></path>
-                    <path d="M3 12H21" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                        stroke-linejoin="round"></path>
-                    <path d="M3 19H21" stroke="white" stroke-width="1.5" stroke-linecap="round"
-                        stroke-linejoin="round"></path>
+                <svg width="24" height="24" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3 5H21" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                    <path d="M3 12H21" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                    <path d="M3 19H21" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
                 </svg>
             </button>
             <div
@@ -24,8 +22,8 @@
                         <span class="mr-3">Product</span>
                         <svg width="10" height="6" viewbox="0 0 10 6" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L5 5L9 1" stroke="currentColor" stroke-width="1.5"
-                                stroke-linecap="round" stroke-linejoin="round"></path>
+                            <path d="M1 1L5 5L9 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"></path>
                         </svg>
                     </div>
                 </a>
@@ -35,9 +33,9 @@
             </div>
             <div class="hidden lg:block ml-auto">
                 <a class="inline-flex items-center justify-center h-10 mr-4 px-4 text-center leading-loose text-sm text-gray-700 hover:text-yellowGreen-700 font-semibold transition duration-200"
-                    href="#">Login</a><a
+                    href="{{ route('login') }}">Login</a><a
                     class="inline-flex items-center justify-center h-10 px-4 text-center leading-loose text-sm text-gray-700 hover:text-yellowGreen-700 font-semibold border border-gray-200 hover:border-yellowGreen-600 shadow-sm hover:shadow-none rounded-lg transition duration-200"
-                    href="#">Get Started</a>
+                    href="{{ route('signup') }}">Get Started</a>
             </div>
         </div>
     </div>
